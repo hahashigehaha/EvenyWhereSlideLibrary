e157bc27efa96e40ba62811acb5dea48fd82d1cb
flyme2012 <446810035@qq.com>
2014-11-19 09:59:05 +0800

    secend commit

--- src/com/flyme/evenywhereslidelibrary/MainActivity.java
+++ src/com/flyme/evenywhereslidelibrary/MainActivity.java
@@ -14,7 +14,65 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_maine);
+		setContentView(R.layout.activity_main);
+		TextView delete = (TextView) findViewById(R.id.one_one);
+		delete.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "delete", 0).show();
+			}
+		});
+		TextView collection = (TextView) findViewById(R.id.one_two);
+		collection.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "collection", 0).show();
+			}
+		});
+		
+		TextView top = (TextView) findViewById(R.id.top_left);
+		top.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "topleft", 0).show();
+			}
+		});
+		TextView top2 = (TextView) findViewById(R.id.top_right);
+		top2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "topright", 0).show();
+			}
+		});
+		TextView delect = (TextView) findViewById(R.id.delect);
+		delect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "delect2", 0).show();
+			}
+		});
+		TextView top_top = (TextView) findViewById(R.id.top_top);
+		top_top.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "top_top", 0).show();
+			}
+		});
+		TextView top_bottom = (TextView) findViewById(R.id.top_bottom);
+		top_bottom.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "top_bottom", 0).show();
+			}
+		});
+		
 		
 	}
 }
